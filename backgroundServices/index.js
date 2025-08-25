2990c97a2f2e1d6f5ba07b043cb860853d55a88b
jayanti mahato <jayantimahato510@gmail.com>
2025-08-25 15:20:17 +0530

    started on backgroundservices

---
+++ backgroundServices/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const cron = require("node-cron");
+const mongoose = require("mongoose");
+dotenv.config();
+
+// db connection
+const DB = process.env.DB;
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("DB connection is successful");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+// task schedular
+const run = () => {
+  cron.schedule("* * * * * *", () => {});
+};
+
+run();
+// server
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Backgroundservices is dancing 🤩 on port  ${PORT}`);
+});
